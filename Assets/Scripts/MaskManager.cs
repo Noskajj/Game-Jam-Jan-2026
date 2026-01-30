150437e17a86b1016f2395010b410ecf51b43377
Raxadore <liam@nellakir.com.au>
2026-01-30 19:46:16 +1100

    Creating Base Scripts
    Settings and Mask Manager

---
+++ Assets/Scripts/MaskManager.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class MaskManager : MonoBehaviour
+{
+    [SerializeField]
+    private List allMasks, currentMasks;
+
+    //Do masks give stat buffs? if so do here
+
+    //Do masks give unique abilities? if so do here
+}
