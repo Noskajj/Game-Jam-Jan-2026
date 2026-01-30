5bb3999001e416c3a1aa7711c9c37d77418c9fd8
Vara112 <elliottvara@gmail.com>
2026-01-30 21:01:13 +1100

    PlayerMovement3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 */
 
+//CONSTANT
+const float BUFFER = 0.02f;
+
 
 
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
@@ -14,19 +17,81 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -20f;
 
     [Header ("Velocity")]
-    public Vel velocity;
+    public Vector3  velocity;
+
+    [Head ("Ground Check Buffer")]
+    float groundCheckBuffer = 0.02f;     //Buffer for ground checks
 
     public bool is_grounded;            //Flag for if on ground
+
+
+    //PREALLOCATING MEMORY
+    private RigidBody _rb;
+    private Collider _col;
+    private float _dt;
+    float distance;
+
+    public Vector3  Momentum => mass * velocity;
+
+
+
 
      
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<Collider>();
+
+        _rb.useGravity = false;                 //We arnt using unity gravity
+
+        _rb.mass = mass;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        is_grounded = _CheckGrounded();
+
+        //Gravity 
+        if (!is_grounded)
+        { 
+            velocity.y += gravity * _dt;         //V = u + a*t
+        }
+        else
+        {
+            //TODO: See if this is needed
+            //Ensures velocity doesnt build up when on ground
+            if (velocity.y < 0f) velocity.y = 0f;
+        }
+
+
+         _rb.MovePosition(_rb.position + velocity * _dt);
+
+
+    }
+
+
+
+    //HELPERS:__________________________
+
+    public void SetVelocity(Vector3 newVelocity)
+    {
+        /*
+        Allows us to update velocity 
+        */
+
+        velocity = newVelocity;
+    } 
+
+    private bool _CheckGrounded()
+    {
         
+        Bounds b = _col.bounds;
+
+        Vector3 origin = new Vector3(b.center.x, b.min.y + BUFFER, b.center.z);
+
+        return Physics.Raycast(origin, Vector3.down ,groundCheckBuffer, 0, QueryTriggerInteraction.Ignore);
+
     }
 }
