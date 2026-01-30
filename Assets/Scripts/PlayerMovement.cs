dbc8567c0d97a8e72952bbdb68b6a30a2c8ec4a5
Vara112 <elliottvara@gmail.com>
2026-01-30 23:59:46 +1100

    Jummp and movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 /*
     GOAL -> 
 
@@ -14,11 +15,21 @@ public class PlayerMovement : MonoBehaviour
     //CONSTANT
     const float BUFFER = 0.02f;
 
+    [SerializeField]
+    private InputActionAsset inputActions;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float gravity = -20f;
 
     public Vector3 velocity;
+
+    public float moveSpeed = 4f;
+
+    public float jumpVel = 8f;
     
     public float mass = 1f;
 
@@ -46,6 +57,12 @@ public class PlayerMovement : MonoBehaviour
      
     void Start()
     {
+        moveAction = inputActions.FindAction("Player/Move");
+        jumpAction = inputActions.FindAction("Player/Jump");
+        
+        jumpAction.performed += Player_Jump;
+
+
         _rb = GetComponent<Rigidbody>();
         _col = GetComponent<Collider>();
 
@@ -53,16 +70,20 @@ public class PlayerMovement : MonoBehaviour
 
         _rb.mass = mass;
 
-        SetVelocity(new Vector3(-4, 5, 0));
+        SetVelocity(new Vector3(0, 5, 0));
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        Player_Move();
+
+
         float _dt = Time.deltaTime;
 
-        is_grounded = CheckGrounded();
+        is_grounded = Check_Grounded();
 
         //Gravity 
         if (!is_grounded)
@@ -87,6 +108,29 @@ public class PlayerMovement : MonoBehaviour
 
     //HELPERS:__________________________
 
+    private void Player_Move()
+    {
+        Vector2 movDirection = moveAction.ReadValue<Vector2>();;
+        if(movDirection.x != 0)
+        {
+            velocity.x = movDirection.x * moveSpeed;
+        }
+        if(movDirection.y != 0)
+        {
+            velocity.z = movDirection.y * moveSpeed;
+        }
+
+    }
+
+    private void Player_Jump(InputAction.CallbackContext context)
+    {
+        float jump = context.ReadValue<float>();
+        if(jump != 0){
+            velocity.y = jumpVel;
+        }
+    }
+
+
     private void Friction(float _dt)
     {
         Vector3 horzVel = new Vector3(velocity.x, 0f, velocity.z);
@@ -99,7 +143,7 @@ public class PlayerMovement : MonoBehaviour
         );
 
         velocity.x = horzVel.x;
-        velocity.y = horzVel.y;
+        velocity.z = horzVel.z;
 
     }
 
@@ -112,7 +156,7 @@ public class PlayerMovement : MonoBehaviour
         velocity = newVelocity;
     } 
     
-    private bool CheckGrounded()
+    private bool Check_Grounded()
     {
         
         Bounds b = _col.bounds;
