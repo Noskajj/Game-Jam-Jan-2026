d11988c2870247fd7e2f8f186d4ed85ab3f5bfc7
riven-delle <marcus.steenberg1@gmail.com>
2026-01-31 21:30:28 +1100

    Implemented Basic UI
    Nav script, MainMenu, Credits. Quit

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour
+{
+    [SerializeField]
+    private float fadeTime = 1f;
+
+    [SerializeField]
+    private Image fadeImg;
+
+
+    public void OpenCreditsScreen()
+    {
+        StartCoroutine(FadeOut("CreditsScreen"));
+    }
+
+    public void OpenMainMenu()
+    {
+        StartCoroutine(FadeOut("MainMenu"));
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(FadeOut("SampleScene"));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator FadeOut(string levelToLoad)
+    {
+        if (fadeImg != null)
+        {
+
+            float timeElapsed = 0f;
+            Color colour = fadeImg.color;
+
+            while (timeElapsed < fadeTime)
+            {
+                Debug.Log("Navigation: time for fade" + timeElapsed);
+                timeElapsed += Time.deltaTime;
+                float timer = timeElapsed / fadeTime;
+
+                colour.a = Mathf.Lerp(0f, 1f, timer);
+
+                fadeImg.color = colour;
+                yield return null;
+            }
+        }
+
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
